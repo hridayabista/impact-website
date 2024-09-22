a197cf8cf5e745ac854408aebd53335934038e62
hridayabista <hridayabistaa@gmail.com>
2024-09-22 11:28:33 +0545

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+let wave1 = document.getElementById('wave1');
+let wave2 = document.getElementById('wave2');
+let wave3 = document.getElementById('wave3');
+let wave4 = document.getElementById('wave4');
+
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+
+    wave1.style.backgroundPositionX = 400 + value * 4 + 'px';
+    wave2.style.backgroundPositionX = 300 + value * -4 + 'px';
+    wave3.style.backgroundPositionX = 200 + value * 2 + 'px';
+    wave4.style.backgroundPositionX = 100 + value * -2 + 'px';
+}) 
+
+$(document).ready(function(){
+    $('#slider').owlCarousel({
+        loop: true,
+        margin: 15,
+        nav: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+});
